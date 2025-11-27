b0323dd8028a3721424268f27c9a2cdd3d0768a8
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 16:45:00 +0700

    tambah function get all siswa

---
+++ app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.myroom.repositori
+
+import com.example.myroom.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
