124194c12321652c3cba0a2203cb70585a0361e9
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 19:21:39 +0700

    tambah interface destinasi navigas

--- app/src/main/java/com/example/myroom/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/myroom/view/route/DestinasiNavigasi.kt
@@ -1,4 +1,14 @@
 package com.example.myroom.view.route
 
 interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resources id yang berisi judul yang akan ditampilkan di layar
+        halaman.
+     */
+    val titleRes: Int
 }
