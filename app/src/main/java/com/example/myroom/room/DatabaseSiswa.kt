1c785a78985436379ccb372f029c99ce6eeef3a0
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 16:37:08 +0700

    tambah room

--- app/src/main/java/com/example/myroom/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroom/room/DatabaseSiswa.kt
@@ -2,6 +2,7 @@ package com.example.myroom.room
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
@@ -13,7 +14,12 @@ abstract class DatabaseSiswa : RoomDatabase(){
         private var Instance: DatabaseSiswa? = null
 
         fun getDatabase(context: Context) : DatabaseSiswa {
-
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, klass = DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also { Instance=it }
+            })
         }
     }
 }
