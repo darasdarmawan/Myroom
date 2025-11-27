957364e5955cdec4f30d38b87004a21935f7527a
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-27 20:29:55 +0700

    tambah function host naviagasi dan siswa app

---
+++ app/src/main/java/com/example/myroom/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,41 @@
+package com.example.myroom.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myroom.view.EntrySiswaScreen
+import com.example.myroom.view.HomeScreen
+import com.example.myroom.view.route.DestinasiEntry
+import com.example.myroom.view.route.DestinasiHome
+
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController(),
+             modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController=navController, startDestination = DestinasiHome.route
+    , modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry
+                    .route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+    }
+}
