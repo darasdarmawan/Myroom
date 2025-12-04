de2f198210c8523d23471b61a670afa0b0f17130
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-04 15:19:38 +0700

    tambah override get dan delete siswa

--- app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
@@ -8,7 +8,7 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
     //edit 1 tambah 3 fungsi berikut
-    fun getSiswaStream(Id: Int): Flow<Siswa>
+    fun getSiswaStream(Id: Int): Flow<Siswa?>
 
     suspend fun deleteSiswa(siswa: Siswa)
 }
@@ -18,5 +18,7 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    }
 }
