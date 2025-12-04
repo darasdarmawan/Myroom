59525ebcb1679a4239891cfd646875434da3cc2e
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-04 15:22:30 +0700

    tambah komonen utama detail siswa screen

--- app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
@@ -20,5 +20,5 @@ class OfflineRepositoriSiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-    }
+
 }
